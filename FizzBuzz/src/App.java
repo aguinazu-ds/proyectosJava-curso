e3ad87169d11bf400e18d5d87cea4067fe4ce9a0
aguinazu-ds <aeguinazu@gmail.com>
2022-05-02 13:10:06 -0400

    Ejercicio FizzBuzz

---
+++ FizzBuzz/src/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int test_int = 60;
+        FizzBuzz fb = new FizzBuzz();
+        System.out.println(fb.fizzBuzz(test_int));
+    }
+}
---
+++ FizzBuzz/src/FizzBuzz.java
@@ -0,0 +1,14 @@
+public class FizzBuzz {
+    public String fizzBuzz(int number) {
+        if (number%15 == 0) {
+            return "FizzBuzz";
+        }
+        if (number%3 == 0) {
+            return "Fizz";
+        }
+        if (number%5 == 0) {
+            return "Buzz";
+        }
+        return Integer.toString(number);
+    }
+}
