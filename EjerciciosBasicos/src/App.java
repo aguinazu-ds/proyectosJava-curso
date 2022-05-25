5027c91f4745746f244ef25e88b56bf1a9798246
aguinazu-ds <aeguinazu@gmail.com>
2022-05-25 17:10:50 -0400

    ejercicios basicos java

--- EjerciciosBasicos/src/App.java
+++ EjerciciosBasicos/src/App.java
@@ -1,5 +1,6 @@
 // import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collections;
 
 import cl.methods.BasicJava;
 
@@ -7,29 +8,47 @@ public class App {
     public static void main(String[] args) throws Exception {
         BasicJava bJ = new BasicJava();
 
-        //bJ.numerosDel1Al255(); // Imprimir numeros del 1 al 255
-        //bJ.numerosImparesDel1Al255(); // Imprimir numeros impares del 1 al 255
+        // bJ.numerosDel1Al255(); // Imprimir numeros del 1 al 255
+        // bJ.numerosImparesDel1Al255(); // Imprimir numeros impares del 1 al 255
         // bJ.sumaDel0Al255(); // suma de los primeros 255 numeros
 
-        //Integer[] myArray = {1,2,3,4,56,678}; 
+        // Integer[] myArray = {1,2,3,4,56,678}; 
         // bJ.recorrerUnArreglo(myArray);
 
-        // int[] test_max = {10,-1,12,-2,-3,0};
-        // System.out.println(bJ.maximo(test_max));
+        // ArrayList<Integer> test_max = new ArrayList<>();
+        // Collections.addAll(test_max, 10,-1,12,-2,-3,0);
+        // System.out.println(test_max);
+        // System.out.println(bJ.maximo(test_max));       
 
-        // Double[] test_promedio = {1.0,2.0,3.0,4.0,5.0,6.0,7.0,8.0,1.5};
+        // ArrayList<Double> test_promedio = new ArrayList<>();
+        // Collections.addAll(test_promedio, 1.0,2.0,3.0,4.0,5.0,6.0,7.0,8.0,1.5);
         // System.out.println("Promedio de: " + bJ.obtenerElPromedio(test_promedio));
 
         // ArrayList<Integer> y = bJ.arregloConNumerosImpares();
         // System.out.println(y);
 
-        // int[] test_mayorqueY = {1,2,3,4,5,6,7,8,9};
-        // System.out.println(bJ.mayorQueY(test_mayorqueY, 10));
+        // ArrayList<Integer> test_mayorqueY = new ArrayList<>();
+        // Collections.addAll(test_mayorqueY, 1,2,3,4,5,6,7,8,9,10,11,12);
+        // System.out.println(bJ.mayorQueY(test_mayorqueY, 4));
 
-        int[] test_valoresalcuadrado = {1,2,3,4};
-        bJ.valoresAlCuadrado(test_valoresalcuadrado);
-        System.out.println(Arrays.toString(test_valoresalcuadrado));
+        // ArrayList<Integer> test_valoresAlCuadrado = new ArrayList<>();
+        // Collections.addAll(test_valoresAlCuadrado, 1,2,3,4);
+        // bJ.valoresAlCuadrado(test_valoresAlCuadrado);
+        // System.out.println(test_valoresAlCuadrado);
         
-        
+        // ArrayList<Integer> test_eliminarNegativos = new ArrayList<>();
+        // Collections.addAll(test_eliminarNegativos, 1,2,3,4,-1,-2,4,-5,-2,-2,1,1,-1,-1,0,-3,4,-1);
+        // System.out.println(test_eliminarNegativos);
+        // bJ.eliminarNumerosNegativos(test_eliminarNegativos);
+        // System.out.println(test_eliminarNegativos);
+
+        // ArrayList<Double> test_minMaxPromedio = new ArrayList<>();
+        // Collections.addAll(test_minMaxPromedio, 1.0,2.0,3.0,4.0,5.0,6.0,7.0,8.0);
+        // System.out.println(bJ.minMaxPromedio(test_minMaxPromedio).toString());
+
+        ArrayList<Integer> test_cambiandoValores = new ArrayList<>();
+        Collections.addAll(test_cambiandoValores, 1,2,3,4,5,5,6,7,8,9,0);
+        bJ.cambiandoValoresArreglo(test_cambiandoValores);
+        System.out.println(test_cambiandoValores);
     }
 }
--- EjerciciosBasicos/src/cl/methods/BasicJava.java
+++ EjerciciosBasicos/src/cl/methods/BasicJava.java
@@ -1,15 +1,17 @@
 package cl.methods;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.ListIterator;
 
 public class BasicJava {
     public void numerosDel1Al255() {
-        for (int i = 1; i < 256; i++) {
+        for (Integer i = 1; i < 256; i++) {
             System.out.println(i);
         }
     }
     public void numerosImparesDel1Al255() {
-        for (int i = 1; i < 256; i++) {
+        for (Integer i = 1; i < 256; i++) {
             if (i%2==1) {
                 System.out.println(i);
             }
@@ -17,7 +19,7 @@ public class BasicJava {
     }
     public void sumaDel0Al255() {
         Integer suma = 0;
-        for (int i = 0; i < 256; i++) {
+        for (Integer i = 0; i < 256; i++) {
              suma = suma + i;
              System.out.println("Nuevo numero: " + i + " Suma: " + suma);
         }
@@ -27,45 +29,73 @@ public class BasicJava {
             System.out.println(item);
         }
     }
-    public int maximo(int[] X) {
-        int max = X[0];
-        for (int i : X) {
+    public int maximo(ArrayList<Integer> X) {
+        Integer max = X.get(0);
+        for (Integer i : X) {
             if (i>=max) {
                 max = i;
             }
         }
         return max;
     }
-    public double obtenerElPromedio(Double[] X) {
+    public double obtenerElPromedio(ArrayList<Double> X) {
         Double suma = 0.0;
-        for (int i = 0; i < X.length; i++) {
-            suma = suma + X[i];
+        for (Integer i = 0; i < X.size(); i++) {
+            suma = suma + X.get(i);
         }
         System.out.println(suma);
-        return (double) suma/X.length;
+        return (double) suma/X.size();
     }
     public ArrayList<Integer> arregloConNumerosImpares() {
         ArrayList<Integer> y = new ArrayList<Integer>();
-        for (int i = 0; i < 256; i++) {
+        for (Integer i = 0; i < 256; i++) {
             if (i%2 == 1) {
                 y.add(i);
             }
         }
         return y;
     }
-    public int mayorQueY(int[] lista, int y) {
-        int contador = 0;
-        for (int numero : lista) {
+    public Integer mayorQueY(ArrayList<Integer> lista, int y) {
+        Integer contador = 0;
+        for (Integer numero : lista) {
             if (numero > y) {
                 contador++;
             }
         }
         return contador;
     }
-    public void valoresAlCuadrado(int[] X) {
-        for (int i = 0; i < X.length; i++) {
-            X[i] = X[i]*X[i];
+    public void valoresAlCuadrado(ArrayList<Integer> X) {
+        for (int i = 0; i < X.size(); i++) {
+            X.set(i, X.get(i)*X.get(i));
         }
     }
-    
+    public void eliminarNumerosNegativos(ArrayList<Integer> X) {
+        ListIterator<Integer> itr = X.listIterator();
+        while (itr.hasNext()) {
+            if (itr.next() < 0) {
+                itr.remove();
+            }
+        }
+    }
+    public ArrayList<Double> minMaxPromedio(ArrayList<Double> X) {
+        Double max = X.get(0);
+        Double min = X.get(0);
+        Double sum = 0.0;
+        for (int i = 0; i < X.size(); i++) {
+            sum = sum + X.get(i);
+            if (X.get(i) > max) {
+                max = X.get(i);
+            } else if (X.get(i) < min) {
+                min = X.get(i);
+            }
+        }
+        ArrayList<Double> min_max_promedio = new ArrayList<>();
+        Collections.addAll(min_max_promedio, min, max, sum/X.size());
+        return min_max_promedio;
+    }
+    public void cambiandoValoresArreglo(ArrayList<Integer> X) {
+        Collections.rotate(X, -1);
+        X.set(X.size() - 1, 0);
+    }
+
 }
